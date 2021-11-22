3f3d946c1ccc48ea38aace65bd6fa59383558cc5
Yudai35 <yuusongben14@gmail.com>
2021-11-22 20:31:24 +0900

    taileindcss練習

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import { darkMode } from "./tailwind.config";
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-<div>
-  bbbbbbbbbbbbbbbbbbbbbb
+<div class="m-8 p-8 bg-gray-200 shadow-lg ">
+  <h1 class="font-bold sm:text-2xl md:text-4xl text-red-700 lg:text-green-700">佑大のIT大学</h1>
+  <ul class="mt-4 space-y-2 list-decimal list-inside text-blue-900" >
+  <li >HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+</ul>
 </div>
 </template>
 
