9b3c506bf2f202d628ce4b8612a267b789d5c9a5
Yudai35 <yuusongben14@gmail.com>
2021-11-23 21:41:04 +0900

    TailwindCss練習

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-<div class="m-8 p-8 bg-gray-200 shadow-lg ">
+<div class="m-8 p-8  shadow-lg ">
   <h1 class="font-bold sm:text-2xl md:text-4xl text-red-700 lg:text-green-700">佑大のIT大学</h1>
   <ul class="mt-4 space-y-2 list-decimal list-inside text-blue-900" >
-  <li >HTML</li>
+  <li>HTML</li>
   <li>CSS</li>
   <li>JavaScript</li>
 </ul>
+  <button class="border-2 border-blue-700 py-2 px-3 hover:text-white hover:bg-blue-700 transition">　
+  ボタン
+</button>
 </div>
 </template>
 
