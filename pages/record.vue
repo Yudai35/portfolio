28908195c29e6b741b1e53d6275aefc3db84688d
Yudai35 <yuusongben14@gmail.com>
2022-01-26 20:58:36 +0900

    メモ機能実装

--- pages/record.vue
+++ pages/record.vue
@@ -4,15 +4,17 @@
   <div class="font-serif px-4">
     <div class="text-center my-12 md:mt-32 md:mb-24">
       <input
-        type="”text”"
-        name="”タイトル”"
+        v-model="title"
+        type="text"
+        name="タイトル"
         placeholder="書籍名"
         class="border-b-2 h-12 w-11/12 md:w-1/2 text-sm md:text-2xl"
       />
     </div>
     <div class="text-center m-auto">
       <textarea
-        name="”メモ欄”"
+        v-model="text"
+        name="メモ欄"
         placeholder="印象に残ったところをアウトプットしよう！"
         id="div"
         cols="30"
@@ -22,7 +24,8 @@
     </div>
     <div class="my-12 md:mt-12 md:mb-20 text-center m-auto">
       <textarea
-        name="”TODOリスト”"
+        v-model="todo"
+        name="TODOリスト"
         placeholder="TODOリスト"
         id="todo"
         cols="30"
@@ -41,6 +44,7 @@
           hover:bg-green-100
           duration-1000
         "
+        @click="save"
       >
         <p class="py-3 text-lg w-52 md:text-2xl">保存</p>
       </button>
@@ -65,5 +69,29 @@
 <script>
 export default {
   layout: "oftenuse",
+  data() {
+    return {
+      title: "",
+      text: "",
+      todo: "",
+    };
+  },
+  methods: {
+    async save() {
+      //保存ボタンを押したときの処理
+      await this.$firestore
+        .collection("memos")
+        .add({
+          createdAt: new Date().getTime(),
+          text: this.title,
+          title: this.title,
+          todo: this.todo,
+          userId: this.$store.state.user.userId,
+        })
+        .then(() => {
+          alert("保存に成功しました！");
+        });
+    },
+  },
 };
 </script>
