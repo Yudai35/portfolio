52995838bf88c4f1b551c1fa0fabf88119f67851
Yudai35 <yuusongben14@gmail.com>
2022-02-19 09:21:52 +0900

    レスポンシブ対応、コードを整えました

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,4 +1,7 @@
+
 <template>
+  <!-- 新規登録 -->
+
   <div class="py-6 bg-gray-100">
     <div
       class="
@@ -30,6 +33,7 @@
         Sign up
       </h2>
       <form @submit.prevent class="" novalidate>
+        <!--👇入力中はエラーメッセージを非表示にしている -->
         <input
           type="”email”"
           name="”email”"
@@ -59,8 +63,6 @@
             h-14
             w-10/12
             bg-green-300
-            hover:bg-green-200
-            duration-1000
             text-center
           "
         >
@@ -68,16 +70,7 @@
         </button>
         <button
           @click="googleLogin"
-          class="
-            text-base
-            my-3
-            md:text-xl
-            h-14
-            w-10/12
-            bg-green-300
-            hover:bg-green-200
-            duration-1000
-          "
+          class="text-base my-3 md:text-xl h-14 w-10/12 bg-green-300"
         >
           Googleで登録
         </button>
@@ -92,7 +85,6 @@
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
@@ -124,23 +116,29 @@ export default {
         });
     },
     register() {
+      //emailにemailRegexpの正規表現の形ではないメールアドレスが入力されたら45行目が発火する
       if (!this.emailRegexp.test(this.user.email)) {
         this.emailErrorMassage =
           "このメールアドレスは無効です。正しく入力してください";
       }
+      //passwordにpasswordRegexpの正規表現の形だはないパスワードが入力されたら49行目が発火する
       if (!this.passwordRegexp.test(this.user.password)) {
         this.passwordErrorMassage =
           "このパスワードは無効です。半角英数字を含んで8-20文字の範囲内で入力してください。";
       }
+      //emailの入力欄に何も入力されていなかったら53行目が発火する
       if (this.user.email === "") {
         this.emailErrorMassage = "メールアドレスを入力してください";
       }
+      //passwordの入力欄に何も入力されていなかったら57行目が発火する
       if (this.user.password === "") {
         this.passwordErrorMassage = "パスワードを入力してください";
       }
+      //emailErrorMassage または passwordErrorMassage に何か当てはまっている時処理を返す
       if (this.emailErrorMassage !== "" || this.passwordErrorMassage !== "") {
         return;
       }
+      //正常に登録できた時の処理
       this.$auth
         .createUserWithEmailAndPassword(this.user.email, this.user.password)
         .then((user) => {
@@ -148,10 +146,15 @@ export default {
           this.$store.dispatch("confirmLogin");
           this.$router.push("/top");
         })
+        //エラーが起きたときの処理
         .catch((error) => {
           console.log({ code: error.code, message: error.message });
+          //👇 auth/invalid-emailの意味
+          //Authに渡したメールアドレスが、メールアドレスの形をしていない場合に出るエラーです。 （例：email : aaaaa）
           if (error.code === "auth/invalid-email") {
             this.emailErrorMassage = "このメールアドレスは適切ではありません";
+            //👇 auth/email-already-in-useの意味
+            //メールアドレスが既に登録済みである場合に出るエラーです。
           } else if (error.code === "auth/email-already-in-use") {
             this.emailErrorMassage = "このメールアドレスは既に登録されています";
           } else {
@@ -159,6 +162,7 @@ export default {
           }
         });
     },
+    //👇入力中はエラーメッセージを非表示にしている
     isInput() {
       this.emailErrorMassage = "";
       this.passwordErrorMassage = "";
