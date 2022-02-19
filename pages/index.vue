6005d9ac4877d2fb93c93fac7a2f3c2aebafcc8f
Yudai35 <yuusongben14@gmail.com>
2022-02-19 09:03:09 +0900

    ファーストビューを挿入しました

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
+  <!-- ログイン前TOPページ -->
   <section>
     <div class="font-serif">
-      <img src="~/assets/Frame 4 (1).png" class="w-screen" />
+      <img src="~/assets/Frame 4 (1).png" />
       <h1 class="mt-20 mb-12 text-center">
         <p class="text-4xl md:text-4xl">NewSelfとは</p>
       </h1>
